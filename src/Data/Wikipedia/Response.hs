1cbf0845ef1b9b14f50d73b25f1bbdfa59eb7701
Adam Wagner <awagner83@gmail.com>
2012-11-25 16:48:26 -0500

    formatting + warning fixes

--- src/Data/Wikipedia/Response.hs
+++ src/Data/Wikipedia/Response.hs
@@ -4,12 +4,7 @@
  -}
 module Data.Wikipedia.Response where
 
-import Control.Applicative
-import Control.Monad
-import Data.Aeson
-import Data.ByteString.Lazy.Char8
 import Data.Map (Map(), unionWith)
-import qualified Data.HashMap.Lazy as H
 import Data.Text (Text)
 
 data Response = Final LinkAssoc
@@ -22,8 +17,8 @@ type OutE      = [Text]
 
 -- | Combine two responses into one.  Result is always a Final
 union :: Response -> Response -> Response
-union (Final a) (Final b) = Final $ unionWith (++) a b
-union (Partial a _) b@(Final _) = union (Final a) b
-union a@(Final _) (Partial b _) = union a (Final b)
+union (Final a) (Final b)         = Final (unionWith (++) a b)
+union (Partial a _) b@(Final _)   = union (Final a)        b
+union a@(Final _) (Partial b _)   = union        a  (Final b)
 union (Partial a _) (Partial b _) = union (Final a) (Final b)
 
